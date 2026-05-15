21d3e45ae85455bf7373a558592018c06e5d0f7a
Rishi Paliwal <141604262+rishipaliwal09@users.noreply.github.com>
2026-05-16 01:26:19 +0530

    update 3

--- client/serve.js
+++ client/serve.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (_req, res) => {
+app.get('/{*path}', (_req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
